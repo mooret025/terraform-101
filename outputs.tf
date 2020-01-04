727918a00bd86da46b4677dffde254af203ccfe5
Tyrone Moore <mooret025@gmail.com>
2020-01-03 15:06:28 -0900

    Added asg and load balancer

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
-  value         = "${aws_instance.example.public_ip}"
-  description   = "The public ip address of the web server"
+  value         = "${aws_lb.example.dns_name}"
+  description   = "The domain name of the load balancer"
 }
